8709de75a5dba8474ad5a8102bf53d549ab14724
rupamhazra <rupamhazra@gmail.com>
2022-03-27 12:30:35 +0530

    modified

--- compare_csv_or_excel.py
+++ compare_csv_or_excel.py
@@ -6,9 +6,21 @@ import pandas as pd
 
 df1 = pd.read_excel('file1.xlsx',engine='openpyxl')
 df2 = pd.read_excel('file2.xlsx', engine='openpyxl')
-print(df1.head(1))
-print(df2.head(1))
+print(df1.head(2))
+print(df2.head(2))
 
 # column names
-print (pd.merge(df1, df2))
+print("=============================================")
+#print (pd.merge(df1, df2))
+
+df3 = pd.merge(df1, df2,how='left')
+
+print(df3)
+
+# Get the unmatched column name
+
+
+# stored to third excel 
+file_name = 'merge_file.xlsx'
+df3.to_excel(file_name)
 
